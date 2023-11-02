f08ffd83dc20f1b6bac43e5f3a56575749685517
Deep-Gohil <142771489+Entity515@users.noreply.github.com>
2023-11-02 20:20:17 +0530

    DG | 02/11/2023 | Ch-1

---
+++ Ch-1/Lecture-1/1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    int a=10,b=20;
+ 
+    printf("Addition Of %d + %d Is       : %d\n",a,b,a+b);
+    printf("Substraction Of %d - %d Is   : %d\n",a,b,a-b);
+    printf("Multiplication Of %d * %d Is : %d\n",a,b,a*b);
+    printf("Division Of %d / %d Is       : %d\n",a,b,a/b);
+    printf("Modulas Of %d %% %d Is        : %d\n",a,b,a%b);
+
+}
