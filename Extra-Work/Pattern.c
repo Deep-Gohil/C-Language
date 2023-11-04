e3de6680e12f1cbbb523d7e9aa320d5c3302cb66
Deep-Gohil <142771489+Entity515@users.noreply.github.com>
2023-11-04 12:12:15 +0530

    Extra-Work

---
+++ Extra-Work/Pattern.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<time.h>
+
+main(){
+	printf("\t***************\n");
+	printf("\t|             |\n");
+	printf("\t|             |\n");
+	printf("\t|             |\n");
+	printf("\t|             |\n");
+	printf("\t***************\n");
+	printf("\t       | \n");
+	printf("\t       |\n");
+	printf("\t     *****\n");
+	
+	sleep(2);
+	system("CLS");
+	printf("\t***************\n");
+	printf("\t| hello       |\n");
+	printf("\t| World       |\n");
+	printf("\t|             |\n");
+	printf("\t|             |\n");
+	printf("\t***************\n");
+	printf("\t       | \n");
+	printf("\t       |\n");
+	printf("\t     *****\n");
+	
+	sleep(2);
+	system("CLS");
+	
+	printf("\t***************\n");
+	printf("\t| Welcome     |\n");
+	printf("\t| To          |\n");
+	printf("\t|             |\n");
+	printf("\t|             |\n");
+	printf("\t***************\n");
+	printf("\t       | \n");
+	printf("\t       |\n");
+	printf("\t     *****\n");
+	
+	sleep(2);
+	system("CLS");
+	
+	printf("\t***************\n");
+	printf("\t| Programing  |\n");
+	printf("\t| World       |\n");
+	printf("\t|             |\n");
+	printf("\t|             |\n");
+	printf("\t***************\n");
+	printf("\t       | \n");
+	printf("\t       |\n");
+	printf("\t     *****\n");
+	
+	sleep(2);
+	system("CLS");
+	
+	printf("\t***************\n");
+	printf("\t| My Name Is: |\n");
+	printf("\t| Deep Gohil  |\n");
+	printf("\t|             |\n");
+	printf("\t|             |\n");
+	printf("\t***************\n");
+	printf("\t       | \n");
+	printf("\t       |\n");
+	printf("\t     *****\n");
+	
+	sleep(2);
+	system("CLS");
+	
+	printf("\t***************\n");
+	printf("\t| I Am 17 Year|\n");
+	printf("\t| Old         |\n");
+	printf("\t|             |\n");
+	printf("\t|             |\n");
+	printf("\t***************\n");
+	printf("\t       | \n");
+	printf("\t       |\n");
+	printf("\t     *****\n");
+	
+	sleep(2);
+	system("CLS");
+	
+	printf("\t***************\n");
+	printf("\t| Course :    |\n");
+	printf("\t| Fullstack   |\n");
+	printf("\t| Developer   |\n");
+	printf("\t|             |\n");
+	printf("\t***************\n");
+	printf("\t       | \n");
+	printf("\t       |\n");
+	printf("\t     *****\n");
+}
