f655fe9e984cb232e18c17744490bd7d429a407b
Deep-Gohil <142771489+Entity515@users.noreply.github.com>
2023-11-20 16:44:00 +0530

    Momentum

---
+++ Momentum/PR-2-1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main(){
+	int n;
+	   printf("Enter Any Number :");
+	      scanf("%d",&n);
+	      
+	    (n%=2)
+				? printf("This Number Is Odd")
+	    		: printf("This Number Is Even");
+	
+}
